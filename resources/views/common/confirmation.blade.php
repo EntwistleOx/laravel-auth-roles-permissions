dc21c7493595d816b6c2eda71012f98cdc0147a7
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-19 14:06:14 -0400

    new style for confirmation messagges in confirmation.blade

--- resources/views/common/confirmation.blade.php
+++ resources/views/common/confirmation.blade.php
@@ -1,5 +1,7 @@
 @if (session('status'))
-    <div class="alert alert-success">
+    <div class="alert alert-success alert-dismissible mt-3">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+        <h4><i class="icon fa fa-ban"></i> Confirmacion!</h4>
         {{ session('status') }}
     </div>
 @endif
