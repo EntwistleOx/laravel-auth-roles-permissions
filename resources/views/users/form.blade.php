ab2389d7548da23838ca5f69fc3975ac28c34caf
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-19 15:19:33 -0400

    Checked some styles

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
     <label for="name">Nombre</label>
-    <input type="text" name="name" class="form-control" aria-describedby="name" placeholder="Nombre" value="{{ $user->name }}"  >
+    <input type="text" name="name" class="form-control" aria-describedby="name" placeholder="Nombre" value="{{ $user->name }}" required>
 </div>
 <div class="form-group">
     <label for="name">Usuario</label>
-    <input type="text" name="username" class="form-control" aria-describedby="username" placeholder="Usuario" value="{{ $user->username }}"  >
+    <input type="text" name="username" class="form-control" aria-describedby="username" placeholder="Usuario" value="{{ $user->username }}" required>
 </div>
