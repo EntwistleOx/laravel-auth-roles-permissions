5cfcca1f331edfe0fb7ac4b748fc7a25446bdb26
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-19 15:12:21 -0400

    Added adminlte styles in roels.index

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -1,62 +1,71 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    Roles
-                    @can('roles.create')
-                    <a href="{{route('roles.create')}}" class="btn btn-primary">Crear</a>
-                    @endcan
-                </div>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Roles<small>it all starts here</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+    </ol>
+</section>
 
-                <div class="card-body">
+<!-- Main content -->
+<section class="content">
+        <!-- Default box -->
+        <div class="box">
+              @include('common.confirmation')
+          <div class="box-header with-border">
+            <h3 class="box-title">Listado de Roles</h3>
 
-                        @include('common.confirmation')
-
-                        <table class="table table-hover table-sm">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Nombre</th>
-                                    <th>Slug</th>
-                                    <th>Descripcion</th>
-                                    <th>Acceso especial</th>
-                                    <th></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($roles as $role)
-                                <tr>
-                                    <td>{{ $role->id }}</td>
-                                    <td>{{ $role->name }}</td>
-                                    <td>{{ $role->slug }}</td>
-                                    <td>{{ $role->description }}</td>
-                                    <td>{{ $role->special }}</td>
-                                    <td class="d-flex justify-content-around">
-                                        @can('roles.edit')
-                                        <a class="btn btn-success" href="{{route('roles.edit',$role->id)}}">Editar</a>
-                                        @endcan
-
-                                        @can('roles.destroy')
-                                        <form action="{{route('roles.destroy',$role->id)}}" method="post">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button class="btn bg-danger text-white" type="submit" onclick="return confirm('¿Desea eliminar el registro?')">Eliminar</button>
-                                        </form>
-                                        @endcan
-                                    </td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-                        {{ $roles->links() }}
-                </div>
-
+            <div class="box-tools pull-right">
+                <a href="{{route('roles.create')}}" class="btn btn-primary btn-sm">Crear</a>
             </div>
+          </div>
+          <div class="box-body table-responsive no-padding">
+              <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Slug</th>
+                            <th>Descripcion</th>
+                            <th>Acceso especial</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($roles as $role)
+                        <tr>
+                            <td>{{ $role->id }}</td>
+                            <td>{{ $role->name }}</td>
+                            <td>{{ $role->slug }}</td>
+                            <td>{{ $role->description }}</td>
+                            <td><span class="label label-warning">{{ $role->special }}</span></td>
+                            <td>
+                                <a class="btn btn-success btn-xs" href="{{route('roles.edit',$role->id)}}">Editar</a>
+                            </td>
+                            <td>
+                            <form action="{{route('roles.destroy',$role->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button class="btn btn-danger btn-xs" type="submit" onclick="return confirm('¿Desea eliminar el registro?')">Eliminar</button>
+                            </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+          </div>
+          <!-- /.box-body -->
+          <div class="box-footer">
+              {{ $roles->links() }}
+          </div>
+          <!-- /.box-footer-->
         </div>
-    </div>
-</div>
+        <!-- /.box -->
+      </section>
+      <!-- /.content -->
 @endsection
