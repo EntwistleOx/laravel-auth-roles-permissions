c31e862c4246534be1a5d2c2e11e4acb6f8591de
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-19 15:01:35 -0400

    Added adminlte styles in users.edit

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,74 +1,94 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    Editar Usuario
-                </div>
-                <div class="card-body">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Usuarios<small>it all starts here</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+    </ol>
+</section>
 
-                    @include('common.errors')
-                    @include('common.confirmation')
+<!-- Main content -->
+<section class="content">
+    <div class="box">
+        <div class="box-header with-border">
+            <h3 class="box-title">Informacion del usuario</h3>
+        </div>
+        <!-- /.box-header -->
+        <form method="POST" action="{{ route('users.update',$user->id) }}">
+        <div class="box-body">
+            @include('common.errors')
+            @include('common.confirmation')
 
-                    <div class="mb-2">
-                        <b>Informacion del usuario</b>
-                    </div>
+            @csrf
+            @method('PATCH')
+            @include ('users.form')
+        </div>
+        <!-- /.box-body -->
 
-                    <form method="POST" action="{{ route('users.update',$user->id) }}">
-                        @csrf
-                        @method('PATCH')
-
-                        @include ('users.form')
-
-                        <hr>
-
-                        <div class="mb-2">
-                            <b>Asignar roles</b>
-                        </div>
-
-                        @foreach ($roles as $role)
-                        <div class="form-group form-check">
-                            <input
-                                type="checkbox"
-                                class="form-check-input"
-                                name="role[]"
-                                value="{{ $role->slug }}"
-                                @foreach($userRoles as $index)
-                                {{ ($index->name == $role->name) ? 'checked' : '' }}
-                                @endforeach
-                            >
-                            <label class="form-check-label">{{ $role->name }}</label>
-                        </div>
+        <div class="box-header">
+            <h3 class="box-title">Asignar Roles</h3>
+        </div>
+        <!-- /.box-header -->
+        <div class="box-body">
+            @foreach ($roles as $role)
+            <div class="checkbox">
+                <label class="check">
+                    <input
+                        type="checkbox"
+                        class="form-check-input"
+                        name="role[]"
+                        value="{{ $role->slug }}"
+                        @foreach($userRoles as $index)
+                        {{ ($index->name == $role->name) ? 'checked' : '' }}
                         @endforeach
+                    >
+                    {{ $role->name }}
+                </label>
+            </div>
+            @endforeach
+        </div>
+        <!-- /.box-body -->
+        <div class="box-footer">
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </div>
+        <!-- box-footer -->
+        </form>
+    </div>
+    <!-- /.box -->
 
-                        <button type="submit" class="btn btn-primary">Guardar</button>
-                    </form>
+    <div class="box box-danger">
+        <div class="box-header with-border">
+            <h3 class="box-title">Cambiar Contraseña</h3>
+        </div>
+        <!-- /.box-header -->
 
-                    <hr>
-
-                    <div class="mb-2">
-                        <b>Cambiar password</b>
-                    </div>
-
-                    <form method="POST" action="{{ route('users.password', $user->id) }}">
-                        @csrf
-                        @method('PATCH')
-                        <div class="form-group">
-                            <label for="name">Nuevo password</label>
-                            <input type="password" name="password" class="form-control" aria-describedby="password" placeholder="Password" required >
-                        </div>
-                        <div class="form-group">
-                            <label for="name">Confirmar password</label>
-                            <input type="password" name="password_confirmation" class="form-control" aria-describedby="password_confirmation " placeholder="Confirmar password" required >
-                        </div>
-                        <button type="submit" class="btn btn-danger">Cambiar</button>
-                    </form>
+        <form method="POST" action="{{ route('users.password', $user->id) }}">
+            <div class="box-body">
+                @csrf
+                @method('PATCH')
+                <div class="form-group">
+                    <label for="password">Nuevo password</label>
+                    <input type="password" name="password" class="form-control" aria-describedby="password" placeholder="Password" required >
+                </div>
+                <div class="form-group">
+                    <label for="passwordConfirm">Confirmar password</label>
+                    <input type="password" name="password_confirmation" class="form-control" aria-describedby="password_confirmation " placeholder="Confirmar password" required >
                 </div>
             </div>
-        </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <button type="submit" class="btn btn-danger">Cambiar</button>
+            </div>
+            <!-- box-footer -->
+        </form>
     </div>
-</div>
+    <!-- /.box -->
+
+</section>
+<!-- /.content -->
+
 @endsection
