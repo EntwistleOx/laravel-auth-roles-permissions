28957584e564339879edf03669b1d573cd28e658
Juan Díaz <juan.diaz.12069@gmail.com>
2019-07-19 17:17:00 -0400

    Added adminlte styles in permissions.edit

--- resources/views/permissions/edit.blade.php
+++ resources/views/permissions/edit.blade.php
@@ -1,26 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header d-flex justify-content-between align-items-center">
-                    Editar Permiso
-                </div>
-                <div class="card-body">
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <h1>Permisos<small>it all starts here</small></h1>
+    <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+        <li><a href="#">Examples</a></li>
+        <li class="active">Blank page</li>
+    </ol>
+</section>
+<!-- Main content -->
+<section class="content">
+        <!-- Default box -->
+        <div class="box">
+            <div class="box-header with-border">
+                <h3 class="box-title">Editar Permiso</h3>
+            </div>
 
+            <form method="POST" action="{{ route('permissions.update', $permission->id) }}">
+                <div class="box-body">
                     @include('common.errors')
                     @include('common.confirmation')
-
-                    <form method="POST" action="{{ route('permissions.update',$permission->id) }}">
-                        @csrf
-                        @method('PATCH')
-                        @include ('permissions.form')
-                    </form>
+                    @csrf
+                    @method('PATCH')
+                    @include ('permissions.form')
                 </div>
+            <!-- /.box-body -->
+            <div class="box-footer">
+                <button type="submit" class="btn btn-primary">Guardar</button>
             </div>
+            <!-- /.box-footer-->
+            </form>
+            <!-- /.form-->
         </div>
-    </div>
-</div>
+        <!-- /.box -->
+    </section>
+    <!-- /.content -->
 @endsection
